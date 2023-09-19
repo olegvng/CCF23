d24684ef4d1e4f179dd042d035061358c7ea9c1c
olegvng <108488217+olegvng@users.noreply.github.com>
2023-09-19 10:16:03 -0400

    Add files via upload

---
+++ bouncing ball/sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //creates random x and y direction
+  xDir = random(-10, 10);
+  yDir = random(-10, 10);
+  //creates sizes of the ball
+  size = random(50, 200);
+  x = random(size, width - size);
+  y = random(size, height - size);
+  noStroke();
+}
+
+function draw() {
+  //adds 
+  background(255, 50);
+  ellipse(x, y, size);
+  shape = random(0,2)
+  //if statement if the with is touched
+  //changes direction, size, and color
+  if (x >= width - size / 2 || x <= size / 2) {
+    xDir *= -1;
+    size = random(50, 200);
+    fill(random(0,255), random(0,255), random(0,255))
+    if (shape == 1){
+      ellipse(x, y, size);
+    }
+    else{
+      rect(x, y, size, size);
+    }
+  }
+  //if statement if the height is touched
+  //changes direction, size, and color
+  if (y >= height - size / 2 || y <= size / 2) {
+    yDir *= -1;
+    size = random(50, 200);
+    fill(random(0,255), random(0,255), random(0,255))
+    if (shape == 1){
+      ellipse(x, y, size);
+    }
+    else{
+      rect(x, y, size, size);
+    }
+  }
+
+  //movement in both direction 
+  x += xDir;
+  y += yDir;
+}
